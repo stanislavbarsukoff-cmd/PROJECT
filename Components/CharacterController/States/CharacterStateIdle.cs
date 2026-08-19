4595075db2a83283fa2a504f5a8db030ebc17120
stan-bars <stanislav_barsukoff@vk.com>
2026-08-19 05:51:52 +0000

    Auto-commit: 2026-08-19 05:51:52

--- Components/CharacterController/States/CharacterStateIdle.cs
+++ Components/CharacterController/States/CharacterStateIdle.cs
@@ -1,8 +1,7 @@
 using Godot;
-using System;
-
 public partial class CharacterStateIdle(
     CharacterController controller,
     CharacterBody3D body)
     : CharacterMoveState<CharacterStateIdle>(controller, body) {
+    
 }
