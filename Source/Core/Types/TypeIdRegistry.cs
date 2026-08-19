ad241220295ec2622d1b5e9792ec8643c72588ce
stan-bars <stanislav_barsukoff@vk.com>
2026-08-19 03:51:00 +0000

    Auto-commit: 2026-08-19 03:51:00

--- Source/Core/Types/TypeIdRegistry.cs
+++ Source/Core/Types/TypeIdRegistry.cs
@@ -1,9 +1,11 @@
+using System.Threading;
+
 public static class TypeIdRegistry<TTag>
 {
     private static int _count;
-    public static int Count = Volatile.Read(_count);
+    public static int Count = Volatile.Read(ref _count);
     public static class For<TEntity>
     {
-        public static int Id { get; } = Interlocked.Increment(_count);
+        public static int Id { get; } = Interlocked.Increment(ref _count);
     }
 }
