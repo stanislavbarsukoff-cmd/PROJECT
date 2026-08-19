81907a05e0c473e9d8fec75171769f0833680834
stanislavbarsukoff-cmd <stanislav.barsukoff@gmail.com>
2026-08-19 13:20:05 +0700

    auto

--- Components/CharacterController/CharacterController.cs
+++ Components/CharacterController/CharacterController.cs
@@ -1,16 +1,16 @@
 using Godot;
 
 public partial class CharacterController : Node {
-    [Export] private CharacterBody3D _body;
-    private StateMachine<ICharacterMoveStateMachine> _stateMachine;
+	[Export] private CharacterBody3D _body;
+	private StateMachine<ICharacterMoveStateMachine> _stateMachine;
 
-    public override void _Ready() {
-        if (_body is null) {
-            _body = GetParent<CharacterBody3D>();
-        }
-        _stateMachine = new StateMachine<ICharacterMoveStateMachine>();
-        _stateMachine.Initialize([
-            new CharacterStateIdle(this, _body)
-        ]);
-    }
+	public override void _Ready() {
+		if (_body is null) {
+			_body = GetParent<CharacterBody3D>();
+		}
+		_stateMachine = new StateMachine<ICharacterMoveStateMachine>();
+		_stateMachine.Initialize([
+			new CharacterStateIdle(this, _body)
+		]);
+	}
 }
