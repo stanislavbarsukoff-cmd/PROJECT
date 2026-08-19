dd8de958edef1b684cd832a1fb76514b167c3ccc
stan-bars <stanislav_barsukoff@vk.com>
2026-08-19 05:15:18 +0000

    Auto-commit: 2026-08-19 05:15:18

--- Components/CharacterController/CharacterMoveState.cs
+++ Components/CharacterController/CharacterMoveState.cs
@@ -1,6 +1,11 @@
 using Godot;
-using System;
-
-public partial class CharacterMoveState : Node
+public interface ICharacterMoveStateMachine;
+public abstract class CharacterMoveState<TState>(
+        CharacterController controller,
+        CharacterBody3D body
+    )
+    : State<ICharacterMoveStateMachine, TState>
 {
+    protected CharacterController _controller = controller;
+    protected CharacterBody3D _body = body;
 }
