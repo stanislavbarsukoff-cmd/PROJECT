ef6dee568e29175b00371def030787ba9de4bf80
stan-bars <stanislav_barsukoff@vk.com>
2026-08-19 05:52:33 +0000

    Auto-commit: 2026-08-19 05:52:33

--- Components/CharacterController/CharacterController.cs
+++ Components/CharacterController/CharacterController.cs
@@ -1,20 +1,17 @@
 using Godot;
 
-public partial class CharacterController : Node
-{
+public partial class CharacterController : Node {
     [Export] private CharacterBody3D _body;
     private StateMachine<ICharacterMoveStateMachine> _stateMachine;
 
 
-    public override void _Ready()
-    {
-        if(_body is null)
-        {
+    public override void _Ready() {
+        if (_body is null) {
             _body = GetParent<CharacterBody3D>();
         }
 
         _stateMachine.Initialize([
-
+            new CharacterStateIdle(this, _body)
         ]);
     }
 }
