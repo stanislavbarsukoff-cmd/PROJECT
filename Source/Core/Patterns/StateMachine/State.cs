5faf4ac46053db642ab9b1f29abd432625e49182
stan-bars <stanislav_barsukoff@vk.com>
2026-08-19 03:51:37 +0000

    Auto-commit: 2026-08-19 03:51:37

--- Source/Core/Patterns/StateMachine/State.cs
+++ Source/Core/Patterns/StateMachine/State.cs
@@ -5,9 +5,9 @@ public abstract class State
 {
     public abstract int Id { get; }
 
-    private virtual void Enter() { }
-    private virtual void Exit() { }
-    private virtual void Process() { }
+    public virtual void Enter() { }
+    public virtual void Exit() { }
+    public virtual void Process() { }
 }
 
 public abstract class State<TMarker, TEntity> : State
