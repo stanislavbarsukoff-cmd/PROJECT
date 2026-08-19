ddbc203c75b622ed8377705eacdb0254d970d939
stan-bars <stanislav_barsukoff@vk.com>
2026-08-19 05:52:38 +0000

    Auto-commit: 2026-08-19 05:52:38

--- Components/CharacterController/CharacterController.cs
+++ Components/CharacterController/CharacterController.cs
@@ -4,7 +4,6 @@ public partial class CharacterController : Node {
     [Export] private CharacterBody3D _body;
     private StateMachine<ICharacterMoveStateMachine> _stateMachine;
 
-
     public override void _Ready() {
         if (_body is null) {
             _body = GetParent<CharacterBody3D>();
