4210f6f880acc6afa38646d5a5dc89b6f4319cad
stan-bars <stanislav_barsukoff@vk.com>
2026-08-19 04:29:40 +0000

    Auto-commit: 2026-08-19 04:29:40

--- Source/Core/Patterns/StateMachine/State.cs
+++ Source/Core/Patterns/StateMachine/State.cs
@@ -1,5 +1,3 @@
-using Godot;
-
 public abstract class State
 {
     public abstract int Id { get; }
@@ -14,38 +12,3 @@ public abstract class State<TMarker, TEntity> : State
     public override int Id => TypeIdRegistry<TMarker>.For<TEntity>.Id;
 }
 
-public class BaseStateMachine
-{
-    public int CurrentStateIndex { get; private set; }
-    public State CurrentState => _states[CurrentStateIndex];
-
-    private State[] _states;
-
-    public void Initialize(State[] states)
-    {
-        if (_states is not null)
-        {
-            return;
-        }
-        for (int i = 0; i < states.Length; i++)
-        {
-            int stateIndex = states[i].Id;
-            if (stateIndex == i)
-            {
-                break;
-            }
-            (states[stateIndex], states[i]) = (states[i], states[stateIndex]);
-            i--;
-        }
-        _states = states;
-        CurrentStateIndex = default;
-    }
-
-    public void SetState<TMarker, TEntity>()
-    {
-        _states[CurrentStateIndex].Exit();
-        CurrentStateIndex = TypeIdRegistry<TMarker>.For<TEntity>.Id;
-        _states[CurrentStateIndex].Enter();
-    }
-
-}
