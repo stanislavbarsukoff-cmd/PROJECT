8b11c39507673c46b4006d2b6a01b7bcbf90d652
stan-bars <stanislav_barsukoff@vk.com>
2026-08-21 07:54:18 +0000

    Auto-commit: 2026-08-21 07:54:18

--- Components/FirstPersonCamera/FirstPersonCamera.cs
+++ Components/FirstPersonCamera/FirstPersonCamera.cs
@@ -1,8 +1,17 @@
 using Godot;
 public partial class FirstPersonCamera : Camera3D {
+    [Export] public float Sensitivity { get; set; }
+
     [Export] private CharacterBody3D _body;
-    [Export]public float Sensitivity { get; set;}
 
     private StateMachine<IFirstPersonCameraStateMachine> _stateMachine;
+
+    public override void _Ready() {
+        _stateMachine = new StateMachine<IFirstPersonCameraStateMachine>();
+        _stateMachine.Initialize([
+           new FirstPersonCameraLookState(this, _body)
+        ]);
+        _stateMachine.SetState<FirstPersonCameraLookState>();
+    }
 
 }
