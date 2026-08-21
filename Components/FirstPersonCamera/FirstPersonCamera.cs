e78a8b1815ad28f8298a2af3da795520bcabf981
stan-bars <stanislav_barsukoff@vk.com>
2026-08-21 07:41:04 +0000

    Auto-commit: 2026-08-21 07:41:04

--- Components/FirstPersonCamera/FirstPersonCamera.cs
+++ Components/FirstPersonCamera/FirstPersonCamera.cs
@@ -1,6 +1,10 @@
 using Godot;
 using System;
 
-public partial class FirstPersonCamera : Node
-{
+public partial class FirstPersonCamera : Camera3D {
+    [Export] private CharacterBody3D _body;
+    [Export]public float Sensitivity { get; set;}
+
+    private StateMachine<IFirstPersonCameraStateMachine> _stateMachine;
+
 }
--- Components/FirstPersonCamera/FirstPersonCameraState.cs
+++ Components/FirstPersonCamera/FirstPersonCameraState.cs
@@ -1,6 +1,9 @@
 using Godot;
-using System;
 
-public partial class FirstPersonCameraState : Node
-{
+public interface IFirstPersonCameraStateMachine;
+public partial class FirstPersonCameraState<TState>(
+    FirstPersonCamera camera, CharacterBody3D body)
+    : State<IFirstPersonCameraStateMachine, TState> {
+    protected FirstPersonCamera _camera = camera;
+    protected CharacterBody3D _body = body;
 }
