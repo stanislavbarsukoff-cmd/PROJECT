b27f1c1e5bd177df9fafcd2090aef3833a50ba5f
stan-bars <stanislav_barsukoff@vk.com>
2026-08-21 02:12:46 +0000

    Auto-commit: 2026-08-21 02:12:46

--- Source/Core/Patterns/StateMachine/State.cs
+++ Source/Core/Patterns/StateMachine/State.cs
@@ -1,13 +1,19 @@
-public abstract class State
-{
-    public abstract int Id { get; }
+using System;
 
-    public virtual void Enter() { }
-    public virtual void Exit() { }
+public abstract class State {
+    public abstract int Id { get; }
+    public event Action Entered;
+    public event Action Exited;
+    public virtual void Enter() {
+        Entered?.Invoke();
+    }
+    public virtual void Exit() {
+        Exited?.Invoke();
+    }
     public virtual void Process() { }
+
 }
 
-public abstract class State<TMarker, TEntity> : State
-{
+public abstract class State<TMarker, TEntity> : State {
     public override int Id => TypeIdRegistry<TMarker>.For<TEntity>.Id;
 }
