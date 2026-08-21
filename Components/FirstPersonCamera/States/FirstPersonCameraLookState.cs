f7ea95fbb555b2a714dfdc7da17271103d4915ed
stan-bars <stanislav_barsukoff@vk.com>
2026-08-21 07:50:04 +0000

    Auto-commit: 2026-08-21 07:50:04

--- Components/FirstPersonCamera/States/FirstPersonCameraLookState.cs
+++ Components/FirstPersonCamera/States/FirstPersonCameraLookState.cs
@@ -1,6 +1,10 @@
 using Godot;
 using System;
 
-public partial class FirstPersonCameraLookState : Node
-{
+public partial class FirstPersonCameraLookState(
+    FirstPersonCamera camera, CharacterBody3D body) 
+    : FirstPersonCameraState<FirstPersonCameraLookState>(camera, body) {
+    private override void Enter() {
+        
+    }
 }
