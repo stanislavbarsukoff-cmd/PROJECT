26526a754405b5c20c56bf7dfa0e0a475390b2db
stan-bars <stanislav_barsukoff@vk.com>
2026-08-21 10:17:58 +0000

    Auto-commit: 2026-08-21 10:17:58

--- Components/FirstPersonCamera/States/FirstPersonCameraLookState.cs
+++ Components/FirstPersonCamera/States/FirstPersonCameraLookState.cs
@@ -5,12 +5,18 @@ public partial class FirstPersonCameraLookState(
     FirstPersonCamera camera, CharacterBody3D body) 
     : FirstPersonCameraState<FirstPersonCameraLookState>(camera, body) {
 
+    private float _xRotation;
+    private float _yRotation;
+
+    private float _xTargetRotation;
+    private float _yTargetRotation;
+
     public override void Enter() {
         GD.Print("c1");
         base.Enter();
     }
 
     public override void Process() {
-        
+        //Vector3 mouseInput = Input.GetAxis("", "", "", "");
     }
 }
