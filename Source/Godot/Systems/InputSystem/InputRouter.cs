975d6b80f0f95b6e813cdd4758778eccc6a2088d
stan-bars <stanislav_barsukoff@vk.com>
2026-08-22 06:27:26 +0000

    Auto-commit: 2026-08-22 06:27:26

--- Source/Godot/Systems/InputSystem/InputRouter.cs
+++ Source/Godot/Systems/InputSystem/InputRouter.cs
@@ -1,11 +1,33 @@
 using Godot;
-using System;
 
-public partial class InputManager : Node {
+public partial class InputRouter : Node {
+    private IInputHandler[] _handlers;
+    public int CurrentIndex { get; private set; }
+    public IInputHandler CurrentHandler { get; private set; }
 
-    
     public override void _Ready() {
-        
+        Initialize([
+
+        ]);
+    }
+
+    public override void _UnhandledInput(InputEvent @event) {
+        CurrentHandler.HandleInput(@event);
+    }
+
+    private void Initialize(IInputHandler[] handlers, int startIndex = 0) {
+        _handlers = handlers;
+        ChangeContextAt(startIndex);
+    }
+
+    public void ChangeContextAt(int index) {
+
+#if DEBUG
+        if (index)
+
+
+            CurrentIndex = index;
+        CurrentHandler = _handlers[index];
     }
 
 }
