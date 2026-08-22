98f140d6112e5c3ad61dc85544a7f1b25a353877
stan-bars <stanislav_barsukoff@vk.com>
2026-08-22 08:11:07 +0000

    Auto-commit: 2026-08-22 08:11:07

--- Source/Godot/Patterns/SingletonNode.cs
+++ Source/Godot/Patterns/SingletonNode.cs
@@ -1,6 +1,6 @@
 using Godot;
 
-public partial class SingletonNode<T> : Node where T: Node {
+public partial class SingletonNode<T> : Node where T : Node {
     public static T Instance { get; private set; }
 
     public override void _EnterTree() {
