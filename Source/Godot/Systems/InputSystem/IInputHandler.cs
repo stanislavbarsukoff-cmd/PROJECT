73e29e03a9850781d87825233ebb8d098d8af181
stan-bars <stanislav_barsukoff@vk.com>
2026-08-22 06:19:38 +0000

    Auto-commit: 2026-08-22 06:19:38

--- Source/Godot/Systems/InputSystem/IInputHandler.cs
+++ Source/Godot/Systems/InputSystem/IInputHandler.cs
@@ -3,27 +3,3 @@ using Godot;
 public interface IInputHandler {
     public void HandleInput(InputEvent @event);
 }
-
-
-public partial class MyInputManager : Node {
-    private IInputContext[] _contexts;
-    public int CurrentIndex { get; private set; }
-    public IInputContext CurrentContext { get; private set; }
-
-    public override void _Ready() {
-        _contexts = [
-          new GameContext()
-        ];
-        ChangeContextAt(0);
-    }
-
-    public void ChangeContextAt(int index) {
-        CurrentIndex = index;
-        CurrentContext = _contexts[index];
-    }
-
-
-    public override void _UnhandledInput(InputEvent @event) {
-        CurrentContext.HandleInput(@event);
-    }
-}
