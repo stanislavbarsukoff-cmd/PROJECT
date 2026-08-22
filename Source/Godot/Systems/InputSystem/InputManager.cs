89bba590760c6d0169f6c9d2abdc64161560d014
stan-bars <stanislav_barsukoff@vk.com>
2026-08-22 03:00:23 +0000

    Auto-commit: 2026-08-22 03:00:23

--- Source/Godot/Systems/InputSystem/InputManager.cs
+++ Source/Godot/Systems/InputSystem/InputManager.cs
@@ -1,6 +1,8 @@
 using Godot;
 using System;
 
-public partial class InputManager : Node
-{
+public partial class InputManager : Node {
+    public override void _Ready() {
+        GD.Print(1);
+    }
 }
