cbace1440f4b260e9b4016b4a7e903f0fd562204
stan-bars <stanislav_barsukoff@vk.com>
2026-08-22 10:29:28 +0000

    Auto-commit: 2026-08-22 10:29:28

--- Features/Player/PlayerRootComposition.cs
+++ Features/Player/PlayerRootComposition.cs
@@ -1,6 +1,13 @@
 using Godot;
 using System;
 
-public partial class PlayerRootComposition : Node
-{
+public partial class PlayerRootComposition : Node {
+    [Export] private CharacterController _controller;
+
+    private InputRouter _input = InputRouter.Instance;
+
+
+    public override void _Ready() {
+        var gameActions = new DefaultActionHandler();
+    }
 }
