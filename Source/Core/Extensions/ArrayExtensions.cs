650d5fdb8788e1dd707562e40b65890f5b0edac1
stan-bars <stanislav_barsukoff@vk.com>
2026-08-22 06:33:08 +0000

    Auto-commit: 2026-08-22 06:33:08

--- Source/Core/Extensions/ArrayExtensions.cs
+++ Source/Core/Extensions/ArrayExtensions.cs
@@ -1,6 +1,10 @@
-using Godot;
-using System;
-
-public partial class ArrayExtensions : Node
-{
+public static class ArrayExtensions {
+    public static bool TryOrGet<T>(this T[] array, int index, out T value) {
+        if (index >= 0 && index < array.Length) {
+            value = array[index];
+            return value is not null;
+        }
+        value = default;
+        return false;
+    }
 }
