79a7cf1f28f5818e652469fff3083eebf47ee6e9
stan-bars <stanislav_barsukoff@vk.com>
2026-08-22 06:12:54 +0000

    Auto-commit: 2026-08-22 06:12:54

--- Source/Godot/Systems/InputSystem/IInputContext.cs
+++ Source/Godot/Systems/InputSystem/IInputContext.cs
@@ -1,6 +1,45 @@
 using Godot;
 using System;
 
-public partial class IInputContext : Node
-{
+public interface IInputContext {
+    public void HandleInput(InputEvent @event);
+}
+
+public class GameContext : IInputContext {
+
+
+    public event Action<Vector2> OnMousePositionChanged;
+
+    public void HandleInput(InputEvent @event) {
+
+    }
+
+}
+
+public enum InputContextEnum {
+    Game = 0
+}
+
+
+public partial class MyInputManager : Node {
+    private IInputContext[] _contexts;
+    public int CurrentIndex { get; private set; }
+    public IInputContext CurrentContext { get; private set; }
+
+    public override void _Ready() {
+        _contexts = [
+          new GameContext()
+        ];
+        ChangeContextAt(0);
+    }
+
+    public void ChangeContextAt(int index) {
+        CurrentIndex = index;
+        CurrentContext = _contexts[index];
+    }
+
+
+    public override void _UnhandledInput(InputEvent @event) {
+        CurrentContext.HandleInput(@event);
+    }
 }
