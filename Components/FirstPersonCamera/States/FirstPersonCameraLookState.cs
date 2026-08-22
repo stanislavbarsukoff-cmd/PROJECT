1876859202bff7d46626a8fc4bd6baff0da4bfda
stan-bars <stanislav_barsukoff@vk.com>
2026-08-22 02:57:14 +0000

    Auto-commit: 2026-08-22 02:57:14

--- Components/FirstPersonCamera/States/FirstPersonCameraLookState.cs
+++ Components/FirstPersonCamera/States/FirstPersonCameraLookState.cs
@@ -2,8 +2,9 @@ using Godot;
 using System;
 
 public partial class FirstPersonCameraLookState(
-    FirstPersonCamera camera, CharacterBody3D body) 
-    : FirstPersonCameraState<FirstPersonCameraLookState>(camera, body) {
+    FirstPersonCamera camera, CharacterBody3D body)
+    : FirstPersonCameraState<FirstPersonCameraLookState>(camera, body)
+{
 
     private float _xRotation;
     private float _yRotation;
@@ -11,12 +12,14 @@ public partial class FirstPersonCameraLookState(
     private float _xTargetRotation;
     private float _yTargetRotation;
 
-    public override void Enter() {
+    public override void Enter()
+    {
         GD.Print("c1");
         base.Enter();
     }
 
-    public override void Process() {
+    public override void Process()
+    {
         //Vector3 mouseInput = Input.GetAxis("", "", "", "");
     }
 }
