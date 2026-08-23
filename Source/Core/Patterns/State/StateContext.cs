d1cf17b004dfb564e774f8ed8def9fcb2bc14f8a
stan-bars <stanislav_barsukoff@vk.com>
2026-08-23 04:46:26 +0000

    Auto-commit: 2026-08-23 04:46:26

--- Source/Core/Patterns/State/StateContext.cs
+++ Source/Core/Patterns/State/StateContext.cs
@@ -1,6 +1,22 @@
 using Godot;
 using System;
 
-public partial class StateContext : Node
-{
+public partial class StateContext<TState, TEnum> : Node
+    where TState : class, IState
+    where TEnum : struct, Enum {
+    private TState[] _states;
+    public TState CurrentState { get; private set; }
+
+    public void Initialize(TState[] states, int startStateIndex = 0) {
+        _states = states;
+        ChangeState(startStateIndex);
+    }
+
+    public void ChangeState(int index) {
+        if (_states.TryGetValue(index, out var state)) {
+            CurrentState?.Exit();
+            (CurrentState = _states[index])?.Enter();
+        }
+    }
+
 }
