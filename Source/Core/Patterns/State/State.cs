062b59e2da9dbf0e6f7da552c9bb92251f37c2ec
stan-bars <stanislav_barsukoff@vk.com>
2026-08-23 05:58:51 +0000

    Auto-commit: 2026-08-23 05:58:51

--- Source/Core/Patterns/State/State.cs
+++ Source/Core/Patterns/State/State.cs
@@ -1,4 +1,9 @@
-public abstract class State : IState {
+using System;
+
+public abstract class State<TContext, TEnum>(TContext context, TEnum type) : IState
+    where TEnum : struct, Enum {
+    protected readonly TContext Context = context;
+    public TEnum StateType { get; } = type;
     public virtual void Enter() { }
     public virtual void Exit() { }
 }
