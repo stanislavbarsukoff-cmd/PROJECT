91127ea841e5782d6808f2d000f4f608c08d1469
stan-bars <stanislav_barsukoff@vk.com>
2026-08-23 05:24:56 +0000

    Auto-commit: 2026-08-23 05:24:56

--- Source/Core/Patterns/State/StateContext.cs
+++ Source/Core/Patterns/State/StateContext.cs
@@ -1,21 +1,29 @@
 using Godot;
 using System;
-
+using System.Runtime.CompilerServices;
 public partial class StateContext<TState, TEnum> : Node
     where TState : class, IState
     where TEnum : struct, Enum {
     private TState[] _states;
     public TState CurrentState { get; private set; }
+    public TEnum CurrentStateType { get; private set; }
 
-    public void Initialize(TState[] states, int startStateIndex = 0) {
+    public void Initialize(TState[] states, int startIndex = 0) {
         _states = states;
-        ChangeState(startStateIndex);
+        ChangeStateAt(startIndex);
     }
-
-    public void ChangeState(int index) {
+    public bool ChangeStateAt(int index) {
         if (_states.TryGetValue(index, out var state)) {
             CurrentState?.Exit();
             (CurrentState = _states[index])?.Enter();
+            return true;
+        }
+        return false;
+    }
+    public void ChangeState(TEnum nextState) {
+        int index = Unsafe.BitCast<TEnum, byte>(nextState);
+        if (ChangeStateAt(index)) {
+            CurrentStateType = nextState;
         }
     }
 
