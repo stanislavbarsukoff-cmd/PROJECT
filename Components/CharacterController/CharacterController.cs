1dcb988c00dddfca41922ae0908c5c76c317e9aa
stan-bars <stanislav_barsukoff@vk.com>
2026-08-23 06:30:08 +0000

    Auto-commit: 2026-08-23 06:30:08

--- Components/CharacterController/CharacterController.cs
+++ Components/CharacterController/CharacterController.cs
@@ -2,9 +2,13 @@ using Godot;
 
 
 public enum MoveState {
-
+	idle = 0
 }
 
-public partial class CharacterController : StateContext<MoveState, > {
+public partial class CharacterController : StateContext<MoveState, CharacterState> {
+	[Export] public CharacterBody3D Body { get; private set; }
 
+    public override void _PhysicsProcess(double delta) {
+        CurrentState.
+    }
 }
--- Components/CharacterController/CharacterState.cs
+++ Components/CharacterController/CharacterState.cs
@@ -1,4 +1,5 @@
 public abstract class CharacterState(
     CharacterController controller, MoveState type)
     : State<CharacterController, MoveState>(controller, type) {
+    public abstract void OnPhysicsProcess(double delta);
 }
