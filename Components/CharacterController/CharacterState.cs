f0b9ffefe191e8e1e7cb2c20fe7e6bf87459a833
stan-bars <stanislav_barsukoff@vk.com>
2026-08-25 12:31:24 +0000

    Auto-commit: 2026-08-25 12:31:24

--- Components/CharacterController/CharacterState.cs
+++ Components/CharacterController/CharacterState.cs
@@ -3,8 +3,8 @@ using Godot;
 namespace Kontur.Components.Character;
 
 public abstract class CharacterState(
-    CharacterController controller, CharacterBody3D body, MoveState type)
-    : State<CharacterController, MoveState>(controller, type) {
+    CharacterController controller, CharacterBody3D body, CharacterStateType type)
+    : State<CharacterController, CharacterStateType>(controller, type) {
     public CharacterBody3D Body { get; } = body;
     public abstract void OnPhysicsProcess(double delta);
 }
