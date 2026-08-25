c7765e6a77ef8bf3ce8c1a124d8a61fd7902b051
stan-bars <stanislav_barsukoff@vk.com>
2026-08-25 12:29:27 +0000

    Auto-commit: 2026-08-25 12:29:27

--- Components/CharacterController/States/IdleState.cs
+++ Components/CharacterController/States/IdleState.cs
@@ -5,7 +5,20 @@ namespace Kontur.Components.Character.States;
 public class IdleState(
     CharacterController controller, CharacterBody3D body)
     : CharacterState(controller, body, MoveState.Idle) {
-    public override void OnPhysicsProcess(double delta) {
 
+    private const float Friction = 20.0f;
+    public override void OnPhysicsProcess(double delta) {
+        float deltaTime = (float)delta;
+        Vector3 velocity = Body.Velocity;
+        if(!Body.IsOnFloor()) {
+            velocity += Body.GetGravity() * deltaTime;
+        }
+        else {
+            float step = deltaTime * Friction;
+            velocity.X = Mathf.MoveToward(velocity.X, default, step);
+            velocity.Z = Mathf.MoveToward(velocity.Z, default, step);
+        }
+        Body.Velocity = velocity;
+        Body.MoveAndSlide();
     }
 }
