acf463e3de79ecf42a4b790dc5ca3d5ddcd33d40
stan-bars <stanislav_barsukoff@vk.com>
2026-08-25 16:52:57 +0000

    Auto-commit: 2026-08-25 16:52:57

--- Features/Player/PlayerInputContext.cs
+++ Features/Player/PlayerInputContext.cs
@@ -1,21 +1,24 @@
 using Godot;
 
 public partial class PlayerInputContext : IActionHandler {
-    private static readonly StringName MoveLeft = "move_left";
-    private static readonly StringName MoveRight = "move_right";
-    private static readonly StringName MoveForward = "move_forward";
-    private static readonly StringName MoveBackward = "move_backward";
+    private static readonly StringName moveLeft = "move_left";
+    private static readonly StringName moveRight = "move_right";
+    private static readonly StringName moveForward = "move_forward";
+    private static readonly StringName moveBackward = "move_backward";
+    private static readonly StringName sprint = "sprint";
 
     public Vector2 MovementVector { get; private set; }
     public bool HasMovementInput { get; private set; }
+    public bool IsSprinting { get; private set; }
 
 
     public void HandleInput(InputEvent @event) {
     }
 
     public void OnPhysicsProcess() {
-        MovementVector = Input.GetVector(MoveLeft, MoveRight, MoveForward, MoveBackward);
+        MovementVector = Input.GetVector(moveLeft, moveRight, moveForward, moveBackward);
         HasMovementInput = MovementVector != Vector2.Zero;
+        IsSprinting = Input.IsActionPressed(sprint); 
     }
 
 }
