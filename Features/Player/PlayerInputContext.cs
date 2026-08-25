b00460fa646ccb825680729ad726a99961171160
stan-bars <stanislav_barsukoff@vk.com>
2026-08-25 13:14:46 +0000

    Auto-commit: 2026-08-25 13:14:46

--- Features/Player/PlayerInputContext.cs
+++ Features/Player/PlayerInputContext.cs
@@ -1,6 +1,16 @@
 using Godot;
-using System;
 
-public partial class PlayerInputContext : Node
-{
+public partial class PlayerInputContext : IActionHandler {
+    private readonly StringName MoveLeft = "move_left";
+    private readonly StringName MoveRight = "move_right";
+    private readonly StringName MoveForward = "move_forward";
+    private readonly StringName MoveBackward = "move_backward";
+
+
+    public void HandleInput(InputEvent @event) {
+        
+    }
+
+    public Vector2 GetMovementVector()
+        => Input.GetVector(MoveLeft, MoveRight, MoveForward, MoveBackward);
 }
