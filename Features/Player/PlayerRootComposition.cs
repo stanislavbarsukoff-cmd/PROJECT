49bdcec99f3feab0db2908a6da83033ea97b68c5
stan-bars <stanislav_barsukoff@vk.com>
2026-08-25 13:41:42 +0000

    Auto-commit: 2026-08-25 13:41:42

--- Features/Player/PlayerRootComposition.cs
+++ Features/Player/PlayerRootComposition.cs
@@ -16,7 +16,7 @@ public partial class PlayerRootComposition : Node {
         _input.Initialize([
             inputContext
         ]);
-        GD.Print(_c);
+        GD.Print(_controller);
         _controller.InputContext = inputContext;
         var idleState = new IdleState(_controller);
         _controller.Initialize([
