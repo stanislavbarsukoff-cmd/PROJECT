cfae41a9d374d1faebdf270301e3bd6b9e5af6d2
stan-bars <stanislav_barsukoff@vk.com>
2026-08-25 15:06:17 +0000

    Auto-commit: 2026-08-25 15:06:17

--- Components/CharacterController/States/IdleState.cs
+++ Components/CharacterController/States/IdleState.cs
@@ -9,13 +9,12 @@ public class IdleState(CharacterController controller)
     private readonly Vector3 _gravity = controller.Body.GetGravity();
 
     public override void OnPhysicsProcess(double delta) {
-    if (!Body.IsOnFloor()) {
-        float deltaTime = (float)delta;
-        Vector3 velocity = Body.Velocity;
-        velocity += _gravity * deltaTime;
-        Body.Velocity = velocity;
-        Body.MoveAndSlide();
-    }
-
+        if (!Body.IsOnFloor()) {
+            float deltaTime = (float)delta;
+            Vector3 velocity = Body.Velocity;
+            velocity += _gravity * deltaTime;
+            Body.Velocity = velocity;
+            Body.MoveAndSlide();
+        }
     }
 }
