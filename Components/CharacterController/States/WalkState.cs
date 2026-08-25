06c750b81503e1a0110d7f195192832e22071d94
stan-bars <stanislav_barsukoff@vk.com>
2026-08-25 16:23:07 +0000

    Auto-commit: 2026-08-25 16:23:07

--- Components/CharacterController/States/WalkState.cs
+++ Components/CharacterController/States/WalkState.cs
@@ -2,6 +2,20 @@ using Godot;
 using System;
 namespace Kontur.Components.Character.States;
 
-public partial class WalkState : Node
-{
+public class WalkState(CharacterController controller)
+    : CharacterState(controller, CharacterStateType.Walk) {
+    private const float Speed = 5.0f;
+    public override void OnPhysicsProcess(double delta) {
+        if (!Context.InputContext.HasMovementInput) {
+            Context.ChangeState(CharacterStateType.Idle);
+            return;
+        }
+        Vector3 velocity = Body.Velocity;
+        Vector2 inputDir = Context.InputContext.MovementVector;
+        Vector3 direction = (Body.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+        velocity.X = direction.X * Speed;
+        velocity.Z = direction.Z * Speed;
+        Body.Velocity = velocity;
+        Body.MoveAndSlide();
+    }
 }
