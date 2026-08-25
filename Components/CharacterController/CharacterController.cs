6e940e01b6b1137ef2f21c5d895acd8aa3313182
stan-bars <stanislav_barsukoff@vk.com>
2026-08-25 12:32:22 +0000

    Auto-commit: 2026-08-25 12:32:22

--- Components/CharacterController/CharacterController.cs
+++ Components/CharacterController/CharacterController.cs
@@ -2,7 +2,7 @@ using Godot;
 
 namespace Kontur.Components.Character;
 
-public partial class CharacterController : StateContext<MoveState, CharacterState> {
+public partial class CharacterController : StateContext<CharacterStateType, CharacterState> {
 	[Export] public CharacterBody3D Body { get; private set; }
 
 	public override void _PhysicsProcess(double delta) {
