ff29d402274f48d0b510b96632dbacb67366f227
stan-bars <stanislav_barsukoff@vk.com>
2026-08-25 15:52:19 +0000

    Auto-commit: 2026-08-25 15:52:19

--- Features/Player/PlayerInputContext.cs
+++ Features/Player/PlayerInputContext.cs
@@ -4,13 +4,20 @@ public partial class PlayerInputContext : IActionHandler {
     private static readonly StringName MoveLeft = "move_left";
     private static readonly StringName MoveRight = "move_right";
     private static readonly StringName MoveForward = "move_forward";
-    private readonly StringName MoveBackward = "move_backward";
+    private static readonly StringName MoveBackward = "move_backward";
+
+    public Vector2 MovementVector { get; private set; }
+    public bool HasMovementInput { get; private set; }
 
 
     public void HandleInput(InputEvent @event) {
-        
+        //13123
+        GD.Print(1);
     }
 
-    public Vector2 MovementVector
-        => Input.GetVector(MoveLeft, MoveRight, MoveForward, MoveBackward);
+    public void OnPhysicsProcess() {
+        MovementVector = Input.GetVector(MoveLeft, MoveRight, MoveForward, MoveBackward);
+        HasMovementInput = MovementVector != Vector2.Zero;
+    }
+
 }
