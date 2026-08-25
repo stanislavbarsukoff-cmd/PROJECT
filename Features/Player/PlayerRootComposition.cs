477869afe18d576b8a151eb57ab3027155905f6e
stanislavbarsukoff-cmd <stanislav.barsukoff@gmail.com>
2026-08-25 20:42:25 +0700

    auto

--- Features/Player/PlayerRootComposition.cs
+++ Features/Player/PlayerRootComposition.cs
@@ -5,22 +5,22 @@ using Kontur.Components.Character;
 using Kontur.Components.Character.States;
 
 public partial class PlayerRootComposition : Node {
-    [Export] private CharacterController _controller;
+	[Export] private CharacterController _controller;
 
-    private InputRouter _input = InputRouter.Instance;
+	private InputRouter _input = InputRouter.Instance;
 
 
-    public override void _Ready() {
-        GD.Print(_input);
-        var inputContext = new PlayerInputContext();
-        _input.Initialize([
-            inputContext
-        ]);
-        GD.Print(_controller);
-        _controller.InputContext = inputContext;
-        var idleState = new IdleState(_controller);
-        _controller.Initialize([
-            idleState
-        ]);
-    }
+	public override void _Ready() {
+		GD.Print(_input);
+		var inputContext = new PlayerInputContext();
+		_input.Initialize([
+			inputContext
+		]);
+		GD.Print(_c);
+		_controller.InputContext = inputContext;
+		var idleState = new IdleState(_controller);
+		_controller.Initialize([
+			idleState
+		]);
+	}
 }
