b9f2b078316779fea147a0d0937e9f07aa4af3ad
stan-bars <stanislav_barsukoff@vk.com>
2026-08-26 12:29:00 +0000

    Auto-commit: 2026-08-26 12:29:00

--- Components/CharacterController/States/IdleState.cs
+++ Components/CharacterController/States/IdleState.cs
@@ -1,22 +1,12 @@
-using Godot;
-
 namespace Kontur.Components.Character.States;
 
 public class IdleState(CharacterController controller)
     : CharacterState(controller, CharacterStateType.Idle) {
-    private readonly Vector3 _gravity = controller.Body.GetGravity();
-
     public override void OnPhysicsProcess(double delta) {
         if (Context.InputContext.HasMovementInput) {
             Context.ChangeState(CharacterStateType.Walk);
             return;
         }
-        if (!Body.IsOnFloor()) {
-            float deltaTime = (float)delta;
-            Vector3 velocity = Body.Velocity;
-            velocity += _gravity * deltaTime;
-            Body.Velocity = velocity;
-            Body.MoveAndSlide();
-        }
+        ApplyGravity(delta);
     }
 }
