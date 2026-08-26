421e374c6d7497e2c8b9bc79991f0507d3bb3fc3
stan-bars <stanislav_barsukoff@vk.com>
2026-08-26 12:26:46 +0000

    Auto-commit: 2026-08-26 12:26:46

--- Components/CharacterController/CharacterState.cs
+++ Components/CharacterController/CharacterState.cs
@@ -5,29 +5,26 @@ namespace Kontur.Components.Character;
 public abstract class CharacterState(
     CharacterController controller, CharacterStateType type)
     : State<CharacterController, CharacterStateType>(controller, type) {
-    
     public CharacterBody3D Body { get; } = controller.Body;
     public abstract void OnPhysicsProcess(double delta);
 
     private const float Acceleration = 15.0f;
     private const float Deceleration = 20.0f;
 
-    protected void MoveWithSpeed(float targetSpeed, double delta) {
+    private readonly Vector3 _gravity = controller.Body.GetGravity();
+    protected void ApplyGravity(double delta) {
+        if (!Body.IsOnFloor()) {
+            Vector3 velocity = Body.Velocity;
+            velocity += _gravity * (float)delta;
+            Body.Velocity = velocity;
+        }
+    }
+
+    protected void MoveWithDirection(Vector3 direction, float targetSpeed, float currentAcceleration, double delta) {
         float deltaTime = (float)delta;
         Vector3 velocity = Body.Velocity;
-        Vector2 inputDir = Context.InputContext.MovementVector;
-        
-        // Возвращаем ваш исходный рабочий вектор направления
-        Vector3 direction = (Body.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-        
-        // Если ввода нет, targetSpeed для расчета торможения должен быть равен 0
-        float currentTargetSpeed = Context.InputContext.HasMovementInput ? targetSpeed : 0f;
-        float step = deltaTime * (Context.InputContext.HasMovementInput ? Acceleration : Deceleration);
-        
-        velocity.X = Mathf.MoveToward(velocity.X, direction.X * currentTargetSpeed, step);
-        velocity.Z = Mathf.MoveToward(velocity.Z, direction.Z * currentTargetSpeed, step);
-        
+        velocity.X = Mathf.MoveToward(velocity.X, direction.X * targetSpeed, currentAcceleration * deltaTime);
+        velocity.Z = Mathf.MoveToward(velocity.Z, direction.Z * targetSpeed, currentAcceleration * deltaTime);
         Body.Velocity = velocity;
-        Body.MoveAndSlide();
     }
 }
