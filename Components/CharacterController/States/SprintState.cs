c8b14afcdd66c1564527c54a95e53509bac14549
stan-bars <stanislav_barsukoff@vk.com>
2026-08-26 12:56:12 +0000

    Auto-commit: 2026-08-26 12:56:12

--- Components/CharacterController/States/SprintState.cs
+++ Components/CharacterController/States/SprintState.cs
@@ -6,18 +6,17 @@ public partial class SprintState(CharacterController controller)
 	: CharacterState(controller, CharacterStateType.Sprint) {
 	private const float SprintSpeed = 10f;
     public override void OnPhysicsProcess(double delta) {
-        if (!Context.InputContext.HasMovementInput) {
-            Context.ChangeState(CharacterStateType.Idle);
-            return;
-        }
-        if (!Context.InputContext.IsSprinting) {
-            Context.ChangeState(CharacterStateType.Walk);
-            return;
-        }
         Vector2 inputDir = Context.InputContext.MovementVector;
         Vector3 direction = (Body.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
         ApplyGravity(delta);
         MoveWithDirection(direction, SprintSpeed, Acceleration, delta);
         Body.MoveAndSlide();
+        if (!Context.InputContext.HasMovementInput) {
+            Context.ChangeState(CharacterStateType.Idle);
+
+        }
+        else if (!Context.InputContext.IsSprinting) {
+            Context.ChangeState(CharacterStateType.Walk);
+        }
     }
 }
