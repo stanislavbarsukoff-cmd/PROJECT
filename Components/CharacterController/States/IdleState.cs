f303282307c40bcf847314b9450bbc4afcb65455
stan-bars <stanislav_barsukoff@vk.com>
2026-08-26 12:56:40 +0000

    Auto-commit: 2026-08-26 12:56:40

--- Components/CharacterController/States/IdleState.cs
+++ Components/CharacterController/States/IdleState.cs
@@ -3,10 +3,9 @@ namespace Kontur.Components.Character.States;
 public class IdleState(CharacterController controller)
     : CharacterState(controller, CharacterStateType.Idle) {
     public override void OnPhysicsProcess(double delta) {
+        ApplyGravity(delta);
         if (Context.InputContext.HasMovementInput) {
             Context.ChangeState(CharacterStateType.Walk);
-            return;
         }
-        ApplyGravity(delta);
     }
 }
